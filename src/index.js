a887746357ee99c1f8984939b58422f26d2bd5e3
Daria Sokolova <dasha-inet@mail.ru>
2019-10-03 19:11:37 +0400

    feat: solution

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    //создадим массив для записи результатов строка/10
+    var arrayOfChars=[];
+    //создадим цикл для извлечения букв
+    for (var index=0; index<expr.length; index+=10){
+        var char=expr.substr(index,10);
+        arrayOfChars.push(char);
+    }
+    //создадим цикл который элементы массива будет декодировать в букву
+    for( var i=0; i<arrayOfChars.length; i+=1){
+        arrayOfChars[i]=GetLetter(arrayOfChars[i]);
+    }
+    let str = arrayOfChars.join('');
+    return str;
 
+}
 module.exports = {
     decode
+}// напишем функцию,которая из 10 цифр получает букву
+function GetLetter(code){
+    if (code==="**********"){
+        return " ";
+    }
+    var morzeCode="";
+    for (var index=0; index<code.length; index+=2){
+        var char=code.substr(index,2);
+        if(char==="10"){
+            morzeCode+=".";
+        }
+        if(char==="11"){
+            morzeCode+="-";
+        }
+    }
+    return MORSE_TABLE[morzeCode]
 }
